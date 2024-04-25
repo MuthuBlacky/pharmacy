e6ef571f98e7a5d826cf907beb9645dbd0ba41f0
MuthuBlacky <muthupandid21cs@psnacet.edu.in>
2024-04-25 20:48:44 +0530

    initial commit

---
+++ app/_components/Header.tsx
@@ -0,0 +1,46 @@
+import {
+  BadgePercent,
+  CircleUserRound,
+  ShoppingCart,
+  UserRound,
+} from "lucide-react";
+import React from "react";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <><div className="w-full flex justify-between p-3">
+      <div className="flex w-[30%] items-center">
+        <Image
+          width={70}
+          height={70}
+          alt="pharmacy-logo"
+          src={"/pharmacy-logo.jpeg"} />
+        <p className="text-2xl font-extrabold">
+          <span className="text-[#f28b82]">T</span>
+          <span className="text-[#fdd663]">r</span>
+          <span className="text-[#8ab4f7]">o</span>
+          <span className="text-[#81c995]">n</span>
+          <span className="text-[#c58af9]">i</span>
+          <span className="text-[#587b82]">x</span>
+        </p>
+      </div>
+      {/* <span className="text-[#f28b82]">T</span>
+      <span className="text-[#fdd663]">r</span>
+      <span className="text-[#8ab4f7]">o</span>
+      <span className="text-[#81c995]">n</span>
+      <span className="text-[#c58af9]">i</span>
+      <span className="text-[#587b82]">x</span> */}
+      <div className="w-[25%] flex items-center justify-evenly">
+        <BadgePercent />
+        <ShoppingCart></ShoppingCart>
+        <button className="w-32 flex justify-around items-center gap-4 rounded-md h-10 border-solid border-[1px] border-black">
+          Login
+          <UserRound className="h-6 text-sm w-6"></UserRound>
+        </button>
+      </div>
+    </div><hr /></>
+  );
+};
+
+export default Header;
---
+++ app/_components/SearchBar.tsx
@@ -0,0 +1,31 @@
+import { Search } from "lucide-react";
+import React from "react";
+import Image from "next/image";
+const SearchBar = () => {
+  return (
+    <div>
+      <div className="w-full flex items-center bg-[#162c2a] h-[200px] mt-4">
+        <img src={"/doctor1.png"} alt="" className="h-full object-contain" />
+        <div className="w-full flex flex-col justify-center items-center h-full">
+          <p className="text-center text-white text-3xl font-semibold mb-10">
+            Buy Medicines and Essentials
+          </p>
+          <div className="w-full h-[28%] relative">
+            <Search
+              size={30}
+              color="#165d59"
+              className="absolute left-3 bottom-[24%] z-10"
+            ></Search>
+            <p className="absolute text-gray-500 left-14 bottom-[25%] z-10">
+              Search Medicines
+            </p>
+            <input type="text" className="w-full h-full rounded-lg z-1" />
+          </div>
+        </div>
+        <img src={"/doctor2.png"} alt="" className="object-contain h-full" />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBar;
